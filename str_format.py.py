3c40d25ac2d1cd4d26ef08684c204c8be5a1ef74
Nicolay <65ngs@mail.ru>
2019-11-19 13:48:26 +0300

    first commit

---
+++ str_format.py.py
@@ -0,0 +1,12 @@
+age = 17
+name = 'Nicolay'
+
+print('Возраст {0} - - {1} лет.'.format(name, age))
+print('Почему {0} забавляется с этим Python?'.format(name))
+
+# второй способ
+age =100
+name = 'Nicolay'
+
+print('Возраст {} - - {} лет.'.format(name,age))
+print('Почему {} забавляется с этим Python?'.format(name))
